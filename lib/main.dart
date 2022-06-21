de14f02a810787c65208877e629d39ccd438ab51
Abdusamad Abdukarimov <abdukarimovabdusamad1998@gmail.com>
2022-06-21 17:44:57 +0500

    add: new screen added

--- lib/main.dart
+++ lib/main.dart
@@ -23,85 +23,248 @@ class MyHomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyColors.C_E5E5E5,
+      backgroundColor: MyColors.white,
       appBar: AppBar(
         centerTitle: true,
-        title: Text(
-          "[0] No meals added",
-          style: MyTextStyles.oswaldMedium500.copyWith(fontSize: 20),
-        ),
         systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarColor: MyColors.C_E5E5E5,
+          statusBarColor: MyColors.white,
           statusBarIconBrightness: Brightness.dark,
           statusBarBrightness: Brightness.light,
         ),
         elevation: 0.0,
-        backgroundColor: MyColors.C_E5E5E5,
+        backgroundColor: MyColors.white,
         leading: Icon(
-          Icons.arrow_back,
+          Icons.menu,
           color: MyColors.black,
         ),
       ),
       body: Column(
         children: [
-          Expanded(
-            flex: 1,
-            child: Center(
-                child: Image.asset(
-              MyIcons.pitsa,
-              fit: BoxFit.cover,
-            )),
-          ),
-          Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Meals Your Way",
-                    style: MyTextStyles.oswaldBold700.copyWith(
-                      fontSize: 40,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    "Pick your own Vegs & Carbs assembles your meals with endles",
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
-                    style: MyTextStyles.oswaldExtraLight300
-                        .copyWith(fontSize: 24, color: Colors.grey),
-                  ),
-                  Expanded(child: SizedBox()),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Add Meal",
-                      textAlign: TextAlign.center,
-                      style: MyTextStyles.oswaldMedium500.copyWith(
-                        fontSize: 25,
-                        color: MyColors.white,
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hey I want....",
+                  style: MyTextStyles.oswaldBold700.copyWith(fontSize: 25),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.set_meal,
+                            size: 36,
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("Salad")
+                        ],
                       ),
                     ),
-                    style: ButtonStyle(
-                        padding: MaterialStateProperty.all<EdgeInsets>(
-                            EdgeInsets.symmetric(horizontal: 36, vertical: 10)),
-                        backgroundColor:
-                        MaterialStateProperty.all(Colors.black),
-                        shape:
-                        MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(28.0),
-                            ))),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.set_meal,
+                            size: 36,
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("Salad")
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.set_meal,
+                            size: 36,
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("Salad")
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.set_meal,
+                            size: 36,
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("Salad")
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.set_meal,
+                            size: 36,
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("Salad")
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(child: getMyContainer()),
+                    Expanded(child: getMyContainer())
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(child: getMyContainer()),
+                    Expanded(child: getMyContainer())
+                  ],
+                ),
+                Expanded(child: SizedBox()),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Add Meal",
+                    textAlign: TextAlign.center,
+                    style: MyTextStyles.oswaldMedium500.copyWith(
+                      fontSize: 25,
+                      color: MyColors.white,
+                    ),
                   ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                ],
-              ))
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.symmetric(horizontal: 36, vertical: 10)),
+                      backgroundColor: MaterialStateProperty.all(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(28.0),
+                      ))),
+                ),
+                SizedBox(
+                  height: 14,
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
   }
+}
+
+Widget getMyContainer() {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          24,
+        ),
+        color: MyColors.C_F3F1F6,
+        boxShadow: [
+          BoxShadow(
+              blurRadius: 4,
+              offset: Offset(0, 3),
+              spreadRadius: 2,
+              color: Colors.grey.shade200),
+        ]),
+    child: Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: SizedBox(),
+            ),
+            Container(
+              height: 25,
+              width: 50,
+              decoration: BoxDecoration(
+                  color: MyColors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(24),
+                      bottomLeft: Radius.circular(24))),
+              child: Center(
+                child: Text(
+                  "\$25",
+                  style: MyTextStyles.oswaldBold700.copyWith(fontSize: 13),
+                ),
+              ),
+            )
+          ],
+        ),
+        Image.asset(
+          MyIcons.pitsa,
+          width: 60,
+          height: 60,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "Navvat Salads",
+          style: MyTextStyles.oswaldRegular400.copyWith(
+            fontSize: 20,
+          ),
+        ),
+        Text(
+          "Opening Bite",
+          style: MyTextStyles.oswaldLight200.copyWith(
+            fontSize: 14,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                Icons.heart_broken,
+                size: 20,
+              ),
+              Text(
+                "4.9",
+                style: MyTextStyles.oswaldBold700.copyWith(
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+      ],
+    ),
+  );
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,5 +4,8 @@ class MyColors{
      static const black =  Colors.black;
      static const white =  Colors.white;
      static const C_2FB209 = Color(0xFF2FB209);
+
+     //F3F1F6
      static const C_E5E5E5 = Color(0xFFE5E5E5);
+     static const C_F3F1F6 = Color(0xFFF3F1F6);
 }
